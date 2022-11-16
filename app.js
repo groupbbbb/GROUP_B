e3204d92bfb40fc71ffaa8966e97206fecf3c2b4
park <goslwjddl@naver.com>
2022-11-16 14:55:59 +0900

    first commit.

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+
+app.set("view engine", "ejs");
+app.use("/views", express.static(__dirname + "/views"));
+app.use("/static", express.static(__dirname + "/static"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
