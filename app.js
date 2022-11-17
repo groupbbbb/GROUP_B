4734d55ca9a48cbe194f198c964ce78a8e6b272c
kangkang1106 <panda3762@naver.com>
2022-11-17 12:01:31 +0900

    signup 틀 완성

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use("/static", express.static(__dirname + "/static"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.render("signup");
+});
+
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
