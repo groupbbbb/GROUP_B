e077c679ed450bc0d85b8419f0ac0d8e9590aebb
park <goslwjddl@naver.com>
2022-11-18 15:54:24 +0900

    navi 페이지 적용 & 반응형 수정 & 카드 반복 for문 추가

--- app.js
+++ app.js
@@ -5,23 +5,28 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.use("/views", express.static(__dirname + "/views"));
+app.use("/common", express.static(__dirname + "/common"));
 app.use("/static", express.static(__dirname + "/static"));
+app.use("/pages", express.static(__dirname + "/pages"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.render("pages/mainpage");
+});
 
-app.get('/', (req,res) => {
-  res.render('pages/main');
-})
+app.get("/upload", (req, res) => {
+  return res.render("pages/upload");
+});
 
-app.get('/upload',(req,res)=>{
-  return res.render('pages/upload');})
+app.get("/login", (req, res) => {
+  res.render("pages/login");
+});
 
-app.get('/login', (req,res) => {
-  res.render('pages/login');
-})
-
+app.get("/signup", (req, res) => {
+  res.render("pages/signup");
+});
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
