0e2516f58c1c3782c697973ce8784204cfae75b0
2hyeseung <leehs226@naver.com>
2022-11-20 18:17:55 +0900

    fix: 코드 정리

--- static/js/postView.js
+++ static/js/postView.js
@@ -1,18 +1,17 @@
-$(function() {
- 
-    $(document).ready(function() {
-      $("#myBtn").click(function() {
-        $("#myModal").css({"display": "block"});});
-      $(".close").click(function() {
-        $("#myModal").css({"display": "none"});});
-      $("html").click(function(event) {
-        if (event.target.id === "myModal") {
-          $("#myModal").css({
-            "display": "none"
-          });}
-      });
-    });
-  })
+// $(function() {
+//     $(document).ready(function() {
+//       $("#myBtn").click(function() {
+//         $("#myModal").css({"display": "block"});});
+//       $(".close").click(function() {
+//         $("#myModal").css({"display": "none"});});
+//       $("html").click(function(event) {
+//         if (event.target.id === "myModal") {
+//           $("#myModal").css({
+//             "display": "none"
+//           });}
+//       });
+//     });
+//   })
   
 
 function postViewOne(obj, id) {
@@ -23,11 +22,12 @@ function postViewOne(obj, id) {
     }).then((res) => {
         return res.data;
     }).then((data) => {
-        document.querySelector('#modal-content').innerHTML=`
-            <span class="close">×</span>
-            <div>게시자 : ${data.user_id}</div>
-            <div>게시일 : ${data.date}</div>
-            <div>내용 : ${data.content}</div>`;
+        // document.querySelector('#modal-content').innerHTML=`
+        //     <span class="close">×</span>
+        //     <div>게시자 : ${data.user_id}</div>
+        //     <div>게시일 : ${data.date}</div>
+        //     <div>내용 : ${data.content}</div>`;
+        console.log(data);
     });
 }
 
