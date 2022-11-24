c6f7f956591541532f0201bb666a18e2fbefc305
kangkang1106 <panda3762@naver.com>
2022-11-24 16:26:12 +0900

    FIXED: chat 2차 기능 추가 구현

--- controller/Cchat.js
+++ controller/Cchat.js
@@ -6,18 +6,10 @@ exports.chat = async (req, res) => {
   let result1 = await models.User.findOne({
     where: { userID: "user1" },
   });
-  let result2 = await models.User.findAll({
-    where: { userID: "user1" },
-    include: [
-      {
-        model: models.User,
-        as: "Followee_id",
-      },
-    ],
-    raw: true,
+  res.render("pages/chat", {
+    isLogin: req.session.user,
+    data1: result1,
   });
-  // console.log("GET /chat result2.userID >>> ", result2);
-  res.render("pages/chat", { data1: result1, data2: result2 });
 };
 
 exports.search = (req, res) => {
