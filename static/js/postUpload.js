73fd05581d5efd1d2b350addc7391ed2d7e0a148
2hyeseung <leehs226@naver.com>
2022-11-25 11:41:52 +0900

    fix: 게시글 빈값 업로드 막음

--- static/js/postUpload.js
+++ static/js/postUpload.js
@@ -4,28 +4,25 @@ function upload(){
     const file = document.getElementById('img');
     const form = document.forms['form_upload'];
 
-    // formData.append('img',file.files[0]);
-    // formData.append('img',file.files[1]);
-    formData.append('content',form.content.value);
-    for(let i=0;i<file.files.length;i++){
-        formData.append('img',file.files[i]);
-    }
-
-    // if(file.files[0]){
-    //     formData.append('img',file.files[0]);  
-    // }
-    // formData.append('content',form.content.value);
-
-    axios({
-        method: 'POST',
-        url : '/post/upload',
-        data : formData,
-        headers: {
-            'Content-Type' : 'multipart/form-data',
+    if(file.files[0]==null || form.content.value==""){
+        alert('모든 값을 입력하세요');
+    }else{
+        formData.append('content',form.content.value);
+        for(let i=0;i<file.files.length;i++){
+            formData.append('img',file.files[i]);
         }
-    }).then(function(res){
-        // console.log(res.data);
-        alert(res.data);
-        document.location.href = '/';
-    })
+        axios({
+            method: 'POST',
+            url : '/post/upload',
+            data : formData,
+            headers: {
+                'Content-Type' : 'multipart/form-data',
+            }
+        }).then(function(res){
+            // console.log(res.data);
+            alert(res.data);
+            document.location.href = '/';
+        })
+
+    }
 }
