817080b245ee24601abd13d3d7a053c5bc618550
kangkang1106 <panda3762@naver.com>
2022-11-28 15:27:58 +0900

    FIXED: nav css

--- static/js/mainpage.js
+++ static/js/mainpage.js
@@ -119,7 +119,7 @@ for (let l = 0; l < box.length; l++) {
       commentBoxesAll[boxclose].style.display = "none";
     }
     for (let listclose = 0; listclose < userListsAll.length; listclose++) {
-      userListsAll[listclose].classList.toggle("active");
+      userListsAll[listclose].style.display = "none";
     }
   });
 }
